171e102f66bd0ec46ab16369b260015beeac55e7
gowthamflm <vengalareddy.challa@gmail.com>
2025-10-30 12:07:05 +0530

    oh my god

--- output.tf
+++ output.tf
@@ -10,9 +10,16 @@ output "public_subnet_ids" {
     value = aws_subnet.public[*].id
 }
 
+# output "public_subnet_ids" {
+#     value = aws_subnet.private[*].id
+# }
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+
+}
+
 output "private_subnet_ids" {
     value = aws_subnet.private[*].id
 }
 
-output "database_subnet_ids" {
-    value = aws_subnet.database[*].id
